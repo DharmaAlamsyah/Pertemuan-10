3beb5a613e84d9e76c629cded81ffacc26191613
MancaMilihya <145315461+DharmaAlamsyah@users.noreply.github.com>
2023-12-10 11:51:06 +0700

    Add files via upload

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 $server = "localhost:8012";
 $user = "root";
 $pass = "";
-$database = "Pertemuan 10";
+$database = "pertemuan 10";
 $conn = mysqli_connect($server, $user, $pass, $database);
 if (!$conn) {
     die("Koneksi ke database gagal: ". mysqli_connect_error());
